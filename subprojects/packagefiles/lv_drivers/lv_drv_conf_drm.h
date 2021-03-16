3985bb747fd6c0bf3a73bee4454067d2db8acdfb
Leif Middelschulte <leif.middelschulte@klsmartin.com>
2021-03-16 17:46:37 +0100

    remove input device support from lv_drivers depedencies

--- subprojects/packagefiles/lv_drivers/lv_drv_conf_drm.h
+++ subprojects/packagefiles/lv_drivers/lv_drv_conf_drm.h
@@ -28,19 +28,4 @@
 #  define DRM_CONNECTOR_ID  -1	/* -1 for the first connected one */
 #endif
 
-/*********************
- *  INPUT DEVICES
- *********************/
-
-/*-------------------------------------------------
- * Touchscreen as libinput interface (for Linux based systems)
- *------------------------------------------------*/
-#ifndef USE_LIBINPUT
-#  define USE_LIBINPUT           1
-#endif
-
-#if USE_LIBINPUT
-#  define LIBINPUT_NAME   "/dev/input/event0"        /*You can use the "evtest" Linux tool to get the list of devices and test them*/
-#endif  /*USE_LIBINPUT*/
-
 #endif  /*LV_DRV_CONF_H*/
